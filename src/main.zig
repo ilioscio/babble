f8fb895f3e8d228a21a9599c1f8864de54040a26
ilioscio <ilioscio@gmail.com>
2026-01-20 03:00:08 -0700

    obfuscated url

--- src/main.zig
+++ src/main.zig
@@ -14,7 +14,7 @@ const HTML_PREFIX =
     \\    <meta charset="UTF-8">
     \\    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     \\    <link rel="icon" type="image/x-icon" href="/favicon.png">
-    \\    <title>Babble</title>
+    \\    <title>Truth</title>
     \\    <meta name="color-scheme" content="light dark">
     \\    <link rel="stylesheet" href="/ilioCSS.min.css">
     \\    <link rel="stylesheet" href="/style.css">
